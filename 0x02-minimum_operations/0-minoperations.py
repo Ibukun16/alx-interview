3649bedac6f209add7892a0ce2b1d3371a99e3c2
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-19 06:23:44 +0100

    Update task to give correct result

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,6 +5,7 @@ can execute only two operations in this file: Copy All and Paste.
 Given a number n, write a method that calculates the fewest number
 of operations needed to result in exactly n H characters in the file.
 """
+import math
 
 
 def minOperations(n):
@@ -15,11 +16,16 @@ def minOperations(n):
     if not isinstance(n, int) or n < 2:
         return 0
 
-    numOps = 0
-
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
     # Start from n and find its factors
-    for c in range(2, n + 1):
+    for c in range(3, int(math.sqrt(n)) + 1, 2):
         while n % c == 0:
-            numOps += c
+            factors.append(c)
             n //= c
+    if n > 2:
+        factors.append(n)
+    numOps = sum(factors)
     return int(numOps)
