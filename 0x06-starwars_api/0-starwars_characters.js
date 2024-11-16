7c9dc355a56be4b32dda08fe370d4bfe38239783
Ibukun16 <ib.agunbiade18@gmail.com>
2024-11-16 09:56:26 +0000

    update changes to task and return correct output

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,23 @@ const request = require('request');
 const filmURL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-	request(`${filmURL}/films/${process.argv[2]}/`, (err, _, body) => {
-		if (err) {
-			console.log(err);
-		}
-		const charactersURL = JSON.parse(body).characters;
-		const characterName = charactersURL.map(
-			url => new Promise((resolve, reject) => {
-				request(url, (promisErr, __, charactersReqBody) => {
-					if (promisErr) {
-						reject(promisErr);
-					}
-					resolve(JSON.parse(charactersReqBody).name);
-				});
-			}));
+  request(`${filmURL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const characterName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promisErr, __, charactersReqBody) => {
+          if (promisErr) {
+            reject(promisErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-		Promise.all(characterName)
-			.then(names => console.log(names.join('\n')))
-			.catch(allErr => console.log(allErr));
-	});
+   Promise.all(characterName)
+     .then(names => console.log(names.join('\n')))
+     .catch(allErr => console.log(allErr));
+});
 }
