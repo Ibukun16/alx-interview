2e6799d837e26eef9a404f51efba284593a6be44
Ibukun16 <ib.agunbiade18@gmail.com>
2024-12-05 06:05:08 +0100

    update changes to project and return correct result

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,6 +2,7 @@
 """Module that computes the the island perimeter
 """
 
+
 def island_perimeter(grid):
     """Computes the perimeter of an island with no lakes.
 
@@ -9,7 +10,7 @@ def island_perimeter(grid):
         grid (_type_): _description_
     """
     perimeter = 0
-    if type(grid) != list:
+    if not isinstance(grid, list):
         return 0
     lg = len(grid)
     for g, row in enumerate(grid):
@@ -20,7 +21,8 @@ def island_perimeter(grid):
             edges = (
                     g == 0 or (len(grid[g - 1]) > r and grid[g - 1][r] == 0),
                     r == lr - 1 or (lr > r + 1 and row[r + 1] == 0),
-                    g == lg - 1 or (len(grid[g + 1]) > r and grid[g + 1][r] == 0),
+                    g == lg - 1 or (len(grid[g + 1]) > r
+                                    and grid[g + 1][r] == 0),
                     r == 0 or row[r - 1] == 0,
                 )
             perimeter += sum(edges)
