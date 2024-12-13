15d27906bff69462ff4fd8f7b10a2aac44e42aea
Ibukun16 <ib.agunbiade18@gmail.com>
2024-12-13 05:18:24 +0100

    Update changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,17 +13,16 @@ def isWinner(x, nums):
 
     # generates primes with a limit of the maximum number in nums
     max_num = max(nums)
-    primes = [True for _ in range(1, max_num + 1)]
-    primes[0] = False
-    for c, is_prime in enumerate(primes, 1):
-        if c == 1 or not is_prime:
-            continue
-        for r in range(c + c, max_num + 1, c):
-            primes[r - 1] = False
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False
+    for c in range(2, int(max_num**0.5) + 1):
+        if primes[c]:
+            for r in range(c**2, max_num + 1, c):
+                primes[r] = False
 
     # remove prime and its multiple from max_num in each round
-    for _, max_num in zip(range(x), nums):
-        count_primes = len(list(filter(lambda x: x, primes[0: max_num])))
+    for num in nums:
+        count_primes = sum(primes[2:num+1])
         ben_wins += count_primes % 2 == 0
         maria_wins += count_primes % 2 == 1
     if maria_wins == ben_wins:
